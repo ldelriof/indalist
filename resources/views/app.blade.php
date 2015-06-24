22f55f500b5ac58df5df9e0a8d863e9ffc7f5172
Luis <ldelriof@gmail.com>
2015-06-24 11:03:32 -0500

    fixed premissions issue

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Laravel</title>
-
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">	
-	<!-- Fonts -->
-	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
