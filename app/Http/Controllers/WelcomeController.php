9ace0d9890b2a7c93e7c856f7a5331560997ea7b
Luis del Rio Francos <luis@iMac-de-Luis.local>
2015-07-20 15:12:39 -0500

    fix

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -39,7 +39,7 @@ class WelcomeController extends Controller {
 		$rand = Video::where('order', '>', -5)->orderByRaw("RAND()")->first();
 		$list = Video::where('active', 0)->where('order', '>', -5)->paginate(50);
 		// $groups = Group::orderBy('id','desc')->paginate(100);
-		$groups = Group::where('private', 0)->orderBy('updated_at','desc')->paginate(100);
+		$groups = Group::where('private', '<', 2)->orderBy('updated_at','desc')->paginate(100);
 
 		$video = Video::where('active', 1)->where('order', '>', -5)->orderBy('order','desc')->orderBy('updated_at','asc')->first();
 
